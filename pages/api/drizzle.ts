01d7f4b20af6fbf99a33752657bf07064ebd36bb
Pranav Kallem <66687154+pkallem@users.noreply.github.com>
2023-07-22 18:55:59 -0400

    Drizzle

---
+++ pages/api/drizzle.ts
@@ -0,0 +1,13 @@
+import {pgTable, serial, text, varchar, integer} from "drizzle-orm/pg-core"
+import { sql } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+
+export const userblogs = pgTable("userblogs", {
+    id: serial("id").primaryKey(),
+    title: varchar("title", { length: 255 }),
+    content: text("content"),
+    user_id: integer("user_id")
+})
+
+export const db = drizzle(sql);
+
--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,29 +1,14 @@
-import { db } from '@vercel/postgres';
-
-const setupDatabase = async () => {
-  const client = await db.connect();
-  try {
-    await client.query(
-      'CREATE TABLE IF NOT EXISTS userblogs (id SERIAL PRIMARY KEY, Title varchar(255), Content text, user_id Integer);'
-    );
-  } catch (error) {
-    console.error('Error setting up database:', error);
-  } finally {
-    client.release();
-  }
-};
-
-setupDatabase();
+import { db, userblogs} from './drizzle';
+import {eq} from "drizzle-orm";
 
 export default async function handler(request, response) {
-  const client = await db.connect();
   try {
     if (request.method === 'GET') {
       if (request.query.id) {
-        const { rows } = await client.query('SELECT * FROM userblogs WHERE id = $1;', [request.query.id]);
+        const rows = await db.select().from(userblogs).where(eq(userblogs.id, request.query.id));
         return response.status(200).json({ blog: rows[0] });
       } else {
-        const { rows } = await client.query('SELECT * FROM userblogs;');
+        const rows = await db.select().from(userblogs);
         return response.status(200).json({ blogs: rows });
       }
     } else if (request.method === 'POST') {
@@ -31,45 +16,28 @@ export default async function handler(request, response) {
       if (!title || !content || !user_id) {
         return response.status(400).json({ error: 'title, content and user_id fields are required.' });
       }
-
-      await client.query('INSERT INTO userblogs (Title, Content, user_id) VALUES ($1, $2, $3);', [
-        title,
-        content,
-        user_id,
-      ]);
+      await db.insert(userblogs).values({ title: title, content : content, user_id: user_id});
       return response.status(200).json({ message: 'Blog created.' });
     } else if (request.method === 'DELETE') {
       const { id, user_id } = request.body;
       if (!id || !user_id) {
         return response.status(400).json({ error: 'id and user_id fields are required.' });
       }
-
-      const { rows } = await client.query('SELECT user_id FROM userblogs WHERE id = $1;', [id]);
-      if (rows[0].user_id != user_id) {
-        return response.status(403).json({ error: 'Unauthorized.' });
-      }
-
-      await client.query('DELETE FROM userblogs WHERE id = $1;', [id]);
+      const rows = await db.select().from(userblogs).where(eq(userblogs.id, id));
+      await db.delete(userblogs).where(eq(userblogs.id, id));
       return response.status(200).json({ message: 'Blog deleted.' });
     } else if (request.method === 'PUT') {
       const { id, newTitle, newContent, user_id } = request.body;
       if (!id || !newTitle || !newContent || !user_id) {
         return response.status(400).json({ error: 'id, newTitle, newContent, and user_id fields are required.' });
       }
-
-      const { rows } = await client.query('SELECT user_id FROM userblogs WHERE id = $1;', [id]);
-      if (rows[0].user_id != user_id) {
-        return response.status(403).json({ error: 'Unauthorized.' });
-      }
-
-      await client.query('UPDATE userblogs SET Title = $1, Content = $2 WHERE id = $3;', [newTitle, newContent, id]);
+      const rows = await db.select().from(userblogs).where(eq(userblogs.id, id));
+      await db.update(userblogs).set({title: newTitle, content: newContent, user_id: user_id}).where(eq(userblogs.id, id));
       return response.status(200).json({ message: 'Blog updated.' });
     } else {
       response.status(405).json({ error: 'Invalid request method' });
     }
   } catch (error) {
     return response.status(500).json({ error });
-  } finally {
-    client.release();
   }
 }
