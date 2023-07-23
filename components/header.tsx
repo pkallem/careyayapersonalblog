c9b6460941779ba9acab5a178ed0f7b1b73a8ba5
Pranav Kallem <66687154+pkallem@users.noreply.github.com>
2023-07-23 15:38:01 -0400

    hamburger menu fixes

--- components/header.tsx
+++ components/header.tsx
@@ -4,77 +4,124 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "../styles/header.module.css";
 
 import { MoonIcon, SunIcon, HamburgerIcon } from '@chakra-ui/icons';
-import { IconButton, useColorMode, useDisclosure, useMediaQuery, Drawer, DrawerBody, Button, VStack } from '@chakra-ui/react';
+import { Box, IconButton, useColorMode, useDisclosure, useMediaQuery, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, Button, VStack, Flex } from '@chakra-ui/react';
 
 export default function Header() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
   const btnRef = useRef(null);
+
   const { data: session, status } = useSession();
+  const loading = status === "loading";
   const { colorMode, toggleColorMode } = useColorMode();
-
-  const DarkModeSwitch = () => (
-    <IconButton
-      aria-label="Toggle dark mode"
-      icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
-      onClick={toggleColorMode}
-      className={styles.button}
-      right={5}
-    />
-  );
-
-  const SignedInText = () => (
-    <span className={styles.signedInText}>
-      <strong>Welcome, {session?.user?.name}</strong>
-      <br />
-      <small>Signed in as {session?.user?.email ?? session?.user?.name}</small>
-    </span>
-  );
 
   return (
     <header>
-      <noscript><style>{`.nojs-show { opacity: 1; top: 0; }`}</style></noscript>
-      <div className={styles.signedInStatus}>
-        <p className={`nojs-show ${!session && status === 'loading' ? styles.loading : styles.loaded}`}>
-          {!session ? (
+      <noscript>
+        <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
+      </noscript>
+      <Flex justifyContent="space-between" alignItems="center" className={styles.signedInStatus}>
+        <p
+          className={`nojs-show ${
+            !session && loading ? styles.loading : styles.loaded
+          }`}
+        >
+          {!session && (
             <>
-              <span className={styles.notSignedInText}>Sign in to create your own blogs</span>
-              <a href="/api/auth/signin" className={styles.buttonPrimary} onClick={(e) => { e.preventDefault(); signIn(); }}>
+              <span className={styles.notSignedInText}>
+                Sign in to create your own blogs
+              </span>
+              <a
+                href={`https://careyayapersonalblog.vercel.app/api/auth/signin`}
+                className={styles.buttonPrimary}
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn();
+                }}
+              >
                 Sign in
               </a>
             </>
-          ) : (
+          )}
+          {session?.user && (
             <>
-              {session?.user?.image && <span style={{ backgroundImage: `url('${session.user.image}')` }} className={styles.avatar} />}
-              <SignedInText />
+              {session.user.image && isLargerThan768 && (
+                <span
+                  style={{ backgroundImage: `url('${session.user.image}')` }}
+                  className={styles.avatar}
+                />
+              )}
+
+              <span className={styles.signedInText}>
+                <strong>Welcome, {session.user.name}</strong>
+                <br />
+                {isLargerThan768 && 
+                  <small>Signed in as {session.user.email ?? session.user.name}</small>
+                }
+              </span>
+
               {isLargerThan768 ? (
                 <>
-                  <a href="/api/auth/signout" className={styles.button} onClick={(e) => { e.preventDefault(); signOut(); }}>
+                  <a
+                    href={`https://careyayapersonalblog.vercel.app/api/auth/signout`}
+                    className={styles.button}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      signOut();
+                    }}
+                  >
                     Sign out
                   </a>
-                  <Link href="/protected"><a className={styles.button}>My Blogs</a></Link>
-                  <Link href="/"><a className={styles.button}>Feed</a></Link>
-                  <DarkModeSwitch />
+
+                  <a
+                    href={`https://careyayapersonalblog.vercel.app/protected`}
+                    className={styles.button}
+                  >
+                    My Blogs
+                  </a>
+                  <a
+                    href={`https://careyayapersonalblog.vercel.app`}
+                    className={styles.button}
+                  >
+                    Feed
+                  </a>
+                  <IconButton
+                    aria-label="Toggle dark mode"
+                    icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
+                    onClick={toggleColorMode}
+                    className={styles.button}
+                    right={5}
+                  />
                 </>
               ) : (
                 <>
                   <IconButton ref={btnRef} colorScheme="teal" onClick={onOpen} aria-label="Options" icon={<HamburgerIcon />} />
                   <Drawer isOpen={isOpen} placement="right" onClose={onClose} finalFocusRef={btnRef}>
-                    <DrawerBody>
-                      <VStack spacing={4}>
-                        <Button onClick={(e) => { e.preventDefault(); signOut(); }}>Sign Out</Button>
-                        <Link href="/protected"><Button>My Blogs</Button></Link>
-                        <Link href="/"><Button>Feed</Button></Link>
-                        <DarkModeSwitch />
-                      </VStack>
-                    </DrawerBody>
+                    <DrawerOverlay>
+                      <DrawerContent>
+                        <DrawerCloseButton />
+                        <DrawerHeader>Welcome, {session.user.name}</DrawerHeader>
+                        <DrawerBody>
+                          <VStack spacing={4}>
+                            <Button onClick={(e) => { e.preventDefault(); signOut(); }}>Sign Out</Button>
+                            <Link href="/protected">
+                              <Button>My Blogs</Button>
+                            </Link>
+                            <Link href="/">
+                              <Button>Feed</Button>
+                            </Link>
+                            <Button onClick={toggleColorMode}>{colorMode === 'dark' ? 'Light Mode' : 'Dark Mode'}</Button>
+                          </VStack>
+                        </DrawerBody>
+                      </DrawerContent>
+                    </DrawerOverlay>
                   </Drawer>
                 </>
               )}
             </>
           )}
         </p>
-      </div>
+      </Flex>
     </header>
   )
 }
