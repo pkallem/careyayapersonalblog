a4dc7660638b0731a520f7cba9e883107578c8c3
Pranav Kallem <66687154+pkallem@users.noreply.github.com>
2023-07-23 17:18:51 -0400

    env variablE

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 export const authOptions = {
-  secret: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.NEXT_PUBLIC_GITHUB_ID,
