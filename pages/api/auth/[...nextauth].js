d79f5db81cf9a6e2d6a5e88ce15e9b9f5b5e3a2c
Pranav Kallem <66687154+pkallem@users.noreply.github.com>
2023-07-23 02:47:57 -0400

    auth issue

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: '0ae75bbfb9e3d585d72e',
+      clientSecret: 'fb24571af8e2647ef10af965f96a6368bd7fc27e'
+    })
+  ],
+  theme: {
+    colorScheme: "dark",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+      }
+      return token
+    },
+    async session({session, token}){
+      if (session.user) {
+        session.user.id = token.id
+      }
+      return session
+    },
+  },
+}
+
+export default NextAuth(authOptions)
